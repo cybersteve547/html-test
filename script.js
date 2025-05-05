9da4893a6a5859875d79ddaa61365f650687d180
cybersteve547 <minecraftstevso@gmail.com>
2025-05-05 14:12:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const myCanvas = document.getElementById("myCanvas");
+const ctx = myCanvas.getContext("2d");
+
+ctx.fillRect(125, 10, 50, 50);
+
 function playSound() {
     var audio = new Audio('AEE.mp3.wav');
     audio.play();
