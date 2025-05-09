41868839194307fff66acbccbb29c3facf4a253b
cybersteve547 <minecraftstevso@gmail.com>
2025-05-09 12:36:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
+const audio = new Audio('AEE.mp3.wav');
 
-ctx.fillRect(125, 100, 50, 50);
+function animate(timestamp) {
+    ctx.clearRect(0, 0, 300, 150);
+    ctx.fillRect(125, 100, 50, 50);
+    requestAnimationFrame(animate);
+}
 
 function playSound() {
-    var audio = new Audio('AEE.mp3.wav');
     audio.play();
 }
+
+requestAnimationFrame(animate);
