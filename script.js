8d6a802a3b569e3ca628f9ebc3e7eccdb2eee2a0
cybersteve547 <minecraftstevso@gmail.com>
2025-05-09 12:43:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 const audio = new Audio('AEE.mp3.wav');
-
+let y = 0;
 function animate(timestamp) {
     ctx.clearRect(0, 0, 300, 150);
-    ctx.fillRect(125, 100, 50, 50);
+    ctx.fillRect(125, 100 - y, 50, 50);
+    y++;
     requestAnimationFrame(animate);
 }
 
